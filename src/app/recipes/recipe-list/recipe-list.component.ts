1ef9ffb289745316c1e59a9b3ea280aaacec6897
booobi <qqwweee59@gmail.com>
2019-09-04 21:07:35 +0300

    move old app, create new recipe app, add planned components, add planned models, test

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe("A test recipe", "This is some description", "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg"),
+    new Recipe("Another test recipe", "This is another description", "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg")
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
